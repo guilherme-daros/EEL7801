ee261954904a45e8569cf3a2065599640c5512a0
Guilherme Daros <gh5777.gh@gmail.com>
2021-10-28 20:53:17 +0100

    changes on raspmcl

--- MCL/RaspberryMCL/RaspberryMCL.py
+++ MCL/RaspberryMCL/RaspberryMCL.py
@@ -4,28 +4,32 @@ from serial import Serial
 from time import sleep
 import paho.mqtt.client as mqtt
 
+print("[MCL] Inicializando")
 fullParkingStateTopic = "0x0000ABCD"
 updateParkingSpotStateTopic = "0xABCD0000"
 
 def on_connect(client, userdata, flags, rc):
     # print("Connected with result code "+str(rc))
     pass
+# def on_publish(client, userdata, mid):
+#  print(f"onPublish: {mid}")
 
 client = mqtt.Client()
 client.on_connect = on_connect
+#client.on_publish = on_publish
 client.connect("mqtt.eclipseprojects.io", 1883, 60)
-
+client.loop()
 db = Database("AngeloniPark")
 ser = Serial('/dev/ttyUSB0')
 
 parkingSpots = Parking(2**3)
-
+print("[MCL] Inicialização Completa")
 while True:
     if not client.is_connected:
         client.connect("mqtt.eclipseprojects.io", 1883, 60)
 
-    line = str(ser.readline(), "UTF-8")
-    # print(line)
+    line = str(ser.readline(),"UTF-8")
+    #print(line)
 
     if ";" in line:
         line = line.split(";")
@@ -34,12 +38,13 @@ while True:
         id = int(id)
         state = int(state)
         line = ""
-        event = "Park" if state == 1 else "Leave"
-
-    print(f"[MCL] Event: {event} on Spot {id}")
-    parkingSpots.parkEvent(id, state)
-    fullParkingStateMessage = f"0;{';'.join(str(e) for e in parkingSpots.spots)}"
-    db.registerParkingEvent(id,state)
-    client.publish(fullParkingStateTopic,fullParkingStateMessage)
-    client.publish(updateParkingSpotStateTopic,f"{id};{state}")
-    client.loop()
+        event = "Estacionou" if state == 1 else "Saiu"
+        print(f"[MCL] Evento: {event} na Vaga {id}")
+        parkingSpots.parkEvent(id, state)
+        fullParkingStateMessage = f"0;{';'.join(str(e) for e in parkingSpots.spots)}"
+        db.registerParkingEvent(id,state)
+        message1 = client.publish(fullParkingStateTopic,fullParkingStateMessage,qos=1)
+        message2 = client.publish(updateParkingSpotStateTopic,f"{id};{state}",qos=1)
+        # print(f"message1.is_published: {message1.is_published()}")
+        # print(f"message2.is_published: {message2.is_published()}")
+        client.loop()
